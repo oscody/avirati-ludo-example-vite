3461612c4411d9f7b8ad605394e8a3115630be9b
bogle <bogleoneil@gmail.com>
2025-06-08 23:39:40 -0400

    Identify cells
    Refactor Walkway component to improve cell rendering logic and add props to Cell component for better data handling.

--- src/containers/Walkway/Container.tsx
+++ src/containers/Walkway/Container.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React from "react";
 import { WALKWAY_LENGTH, WALKWAY_WIDTH } from '../../globalConstants';
 import { WalkwayPosition } from '../../state/interfaces';
 import { getStyleObject } from '../../utils';
@@ -11,46 +10,63 @@ interface IWalkwayProps {
   position: WalkwayPosition;
 }
 
-
-
-
 const Walkway: React.FC<IWalkwayProps> = ({ position }) => {
 
-  const isHorizontal = [
-    WalkwayPosition.EAST,
-    WalkwayPosition.WEST,
-  ].includes(position);
+  const isHorizontalWalkway = React.useCallback(
+    () => [WalkwayPosition.EAST, WalkwayPosition.WEST].includes(position),
+    [position]
+  );
 
-
-  const  renderCells = () => {
+  // This replaces the class's renderCells method
+  const renderCells = React.useCallback(() => {
+    const isHorizontal = isHorizontalWalkway();
     const cells = [];
 
-    for (let i = 0; i < WALKWAY_LENGTH * WALKWAY_WIDTH; i++) {
-      cells.push(
-        <Cell key={i}/>,
-      )
+    if (isHorizontal) {
+      for (let row = 0; row < WALKWAY_WIDTH; row++) {
+        for (let column = 0; column < WALKWAY_LENGTH; column++) {
+          cells.push(
+            <Cell
+              key={`cell-${row}-${column}`}
+              column={column}
+              row={row}
+              walkwayPosition={position}
+            />
+          );
+        }
+      }
+    } else {
+      for (let row = 0; row < WALKWAY_LENGTH; row++) {
+        for (let column = 0; column < WALKWAY_WIDTH; column++) {
+          cells.push(
+            <Cell
+              key={`cell-${row}-${column}`}
+              column={column}
+              row={row}
+              walkwayPosition={position}
+            />
+          );
+        }
+      }
     }
 
     return cells;
-  }
+  }, [isHorizontalWalkway, position]);
+
+  const isHorizontal = isHorizontalWalkway();
 
   return (
     <div
       className={styles.Container}
       style={
-          isHorizontal
+        isHorizontal
           ? getStyleObject(WALKWAY_LENGTH, WALKWAY_WIDTH)
           : getStyleObject(WALKWAY_WIDTH, WALKWAY_LENGTH)
-  
-      }>
-        {
-          renderCells()
-        }
+      }
+    >
+      {renderCells()}
     </div>
   );
-
-
-  
 };
 
 export { Walkway };
--- src/containers/Walkway/components/Cell.tsx
+++ src/containers/Walkway/components/Cell.tsx
@@ -4,12 +4,23 @@ import { CELL_SIZE } from "../../../globalConstants";
 import { getStyleObject } from "../../../utils";
 
 import styles from "./Cell.module.css";
+import { WalkwayPosition } from '../../../state/interfaces';
 
-export const Cell = () => {
+
+interface ICellProps {
+  row: number;
+  column: number;
+  walkwayPosition: WalkwayPosition;
+}
+
+export const Cell = ({ column, row, walkwayPosition }: ICellProps) => {
   return (
     <div
       className={styles.Container}
       style={getStyleObject(CELL_SIZE, CELL_SIZE)}
+      data-row={row}
+      data-column={column}
+      data-position={walkwayPosition}
     />
   );
 };
